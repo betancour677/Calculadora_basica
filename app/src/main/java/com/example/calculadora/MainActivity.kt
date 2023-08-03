f6ed40607321c238ba13649c109970d4de2c2170
emmanuel <emmanuel.betancour@gmail.com>
2023-08-02 22:19:07 -0400

    Kotlin vs1

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,122 +1,87 @@
-package com.example.calculadora;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-public class MainActivity extends AppCompatActivity {
-
-    private EditText num1EditText, num2EditText;
-    private TextView resultTextView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        num1EditText = findViewById(R.id.et_primernum);
-        num2EditText = findViewById(R.id.et_segnum);
-        resultTextView = findViewById(R.id.et_resultado);
-
-        Button addBtn = findViewById(R.id.suma);
-        addBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    package com.example.calculadora
+    
+    import android.os.Bundle
+    import android.widget.Button
+    import android.widget.EditText
+    import android.widget.TextView
+    import androidx.appcompat.app.AlertDialog
+    import androidx.appcompat.app.AppCompatActivity
+    
+    class MainActivity : AppCompatActivity() {
+        private var num1EditText: EditText? = null
+        private var num2EditText: EditText? = null
+        private var resultTextView: TextView? = null
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            num1EditText = findViewById(R.id.et_primernum)
+            num2EditText = findViewById(R.id.et_segnum)
+            resultTextView = findViewById(R.id.et_resultado)
+            val addBtn = findViewById<Button>(R.id.suma)
+            addBtn.setOnClickListener {
                 if (validateFields()) {
-                    performOperation('+');
+                    performOperation('+')
                 } else {
-                    showAlert("Por favor, complete ambos campos.");
+                    showAlert("Por favor, complete ambos campos.")
                 }
             }
-        });
-
-        Button subtractBtn = findViewById(R.id.resta);
-        subtractBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            val subtractBtn = findViewById<Button>(R.id.resta)
+            subtractBtn.setOnClickListener {
                 if (validateFields()) {
-                performOperation('-');
-            } else {
-                showAlert("Por favor, complete ambos campos.");
-            }
-            }
-
-
-        });
-
-        Button multiplyBtn = findViewById(R.id.multiplicacion);
-        multiplyBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (validateFields()) {
-                    performOperation('*');
+                    performOperation('-')
                 } else {
-                    showAlert("Por favor, complete ambos campos.");
+                    showAlert("Por favor, complete ambos campos.")
                 }
             }
-        });
-
-        Button divideBtn = findViewById(R.id.division);
-        divideBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            val multiplyBtn = findViewById<Button>(R.id.multiplicacion)
+            multiplyBtn.setOnClickListener {
                 if (validateFields()) {
-                    performOperation('/');
+                    performOperation('*')
                 } else {
-                    showAlert("Por favor, complete ambos campos.");
+                    showAlert("Por favor, complete ambos campos.")
                 }
             }
-        });
-
-        Button exitBtn = findViewById(R.id.exitBtn);
-        exitBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
+            val divideBtn = findViewById<Button>(R.id.division)
+            divideBtn.setOnClickListener {
+                if (validateFields()) {
+                    performOperation('/')
+                } else {
+                    showAlert("Por favor, complete ambos campos.")
+                }
             }
-        });
-    }
-        private boolean validateFields() {
-            String num1 = num1EditText.getText().toString().trim();
-            String num2 = num2EditText.getText().toString().trim();
-
-            return !num1.isEmpty() && !num2.isEmpty();
+            val exitBtn = findViewById<Button>(R.id.exitBtn)
+            exitBtn.setOnClickListener { finish() }
         }
-    private void performOperation(char operator) {
-        double num1 = Double.parseDouble(num1EditText.getText().toString());
-        double num2 = Double.parseDouble(num2EditText.getText().toString());
-        double result = 0;
-
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    resultTextView.setText("Error: División entre cero");
-                    return;
-                }
-                break;
+    
+        private fun validateFields(): Boolean {
+            val num1 = num1EditText!!.text.toString().trim { it <= ' ' }
+            val num2 = num2EditText!!.text.toString().trim { it <= ' ' }
+            return !num1.isEmpty() && !num2.isEmpty()
         }
-
-        resultTextView.setText(String.valueOf(result));
-    }
-    private void showAlert(String message) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Alerta")
+    
+        private fun performOperation(operator: Char) {
+            val num1 = num1EditText!!.text.toString().toDouble()
+            val num2 = num2EditText!!.text.toString().toDouble()
+            var result = 0.0
+            when (operator) {
+                '+' -> result = num1 + num2
+                '-' -> result = num1 - num2
+                '*' -> result = num1 * num2
+                '/' -> if (num2 != 0.0) {
+                    result = num1 / num2
+                } else {
+                    resultTextView!!.text = "Error: División entre cero"
+                    return
+                }
+            }
+            resultTextView!!.text = result.toString()
+        }
+    
+        private fun showAlert(message: String) {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Alerta")
                 .setMessage(message)
                 .setPositiveButton("Aceptar", null)
-                .show();
+                .show()
+        }
     }
-}
